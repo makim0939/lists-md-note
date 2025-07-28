cb51e06f3e835f391f13dce12556377fb0de442a
makim0939 <makim0939@gmail.com>
2025-07-28 17:29:42 +0900

    SharePointのList操作と認証処理を分離; DviceCode認証を実装

---
+++ WriteNippoLocally/WriteNippoLocally/WriteNippoLocally/Model/SharePointAuthService.cs
@@ -0,0 +1,56 @@
+﻿using dotenv.net;
+using Microsoft.SharePoint.Client;
+using PnP.Framework;
+using PnP.PowerShell.Commands.Utilities;
+
+namespace WriteNippoLocally.Model
+{
+    internal class SharePointAuthService
+    {
+        private class Credentials
+        {
+            public string ClientId { get; private set; }
+            public string TenantId { get; private set; }
+            public string ClientSecret { get; private set; }
+
+            public Credentials()
+            {
+                DotEnv.Load();
+                string? clientId = Environment.GetEnvironmentVariable("CLIENT_ID");
+                string? tenantId = Environment.GetEnvironmentVariable("TENANT_ID");
+                string? clientSecret = Environment.GetEnvironmentVariable("CLIENT_SECRET");
+
+                // 認証情報がなかった場合はエラー
+                if (clientId == null || tenantId == null || clientSecret == null)
+                {
+                    throw new Exception();
+                }
+                ClientId = clientId;
+                TenantId = tenantId;
+                ClientSecret = clientSecret;
+            }
+        }
+
+        public SharePointAuthService() { }
+
+        // 
+        // Microsoft EntraIDでのアプリ登録不要
+        // 環境によっては使えない。
+        public static ClientContext ExecuteAuth(string siteUrl)
+        {
+            ClientContext context = BrowserHelper.GetWebLoginClientContext(siteUrl, true);
+            return context;
+        }
+        
+        // DeviceCode認証
+        // Microsoft EntraIDでのアプリ登録必須
+        public static async Task<ClientContext> ExecuteDeviceCodeAuth(string siteUrl)
+        {
+            var credentials = new Credentials();
+            var authManager = new AuthenticationManager(credentials.ClientId, "http://localhost", credentials.TenantId);
+            using var context = await authManager.GetContextAsync(siteUrl);
+
+            return context;
+        }
+    }
+}
