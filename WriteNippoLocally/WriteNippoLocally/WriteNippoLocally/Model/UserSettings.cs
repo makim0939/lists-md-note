4a991309851226dfb216331d2935b40ef903ccf5
makim0939 <makim0939@gmail.com>
2025-07-28 17:22:32 +0900

    UserSettingsの取得メソッドをUtilServiceからUserSettingsに移動

--- WriteNippoLocally/WriteNippoLocally/WriteNippoLocally/Model/UserSettings.cs
+++ WriteNippoLocally/WriteNippoLocally/WriteNippoLocally/Model/UserSettings.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
+using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
+using System.Web;
 
 namespace WriteNippoLocally.Model
 {
@@ -17,5 +15,60 @@ namespace WriteNippoLocally.Model
 
         [JsonInclude]
         public string FileNameFormat { get; set; } = "日報yyyy-mm-dd";
+
+        public UserSettings() { }
+
+        // ユーザ設定を取得
+        public static UserSettings GetUserSettings()
+        {
+            UserSettings? settings;
+            try
+            {
+                string settingsStr = File.ReadAllText(@".\UserSettings.json");
+                settings = JsonSerializer.Deserialize<UserSettings>(settingsStr);
+            }
+            catch
+            {
+                settings = null;
+            }
+
+            if (settings == null)
+            {
+                // ファイルが読み取れない場合はデフォルト設定
+                settings = new UserSettings();
+            }
+
+            return settings;
+        }
+
+        //  "https://[company].sharepoint.com/sites/[site]/"の部分を取得
+        public string GetSharePointUrl()
+        {
+            Uri uri = new Uri(this.SiteUrl);
+            return uri.AbsoluteUri.Split("Lists")[0];
+        }
+
+        // "sites/[site]/Lists/[list]/AllItems.aspx"の部分を取得
+        public string GetSitePath()
+        {
+            Uri uri = new Uri(this.SiteUrl);
+            return uri.AbsolutePath;
+        }
+
+        // URLのパラメータからviewidを取得
+        public string? GetViewId()
+        {
+            Uri uri = new Uri(this.SiteUrl);
+            string? viewId = null;
+            var prams = HttpUtility.ParseQueryString(uri.Query);
+            foreach (string? key in prams.AllKeys)
+            {
+                if (key == "viewid")
+                {
+                    viewId = prams[key];
+                }
+            }
+            return viewId;
+        }
     }
 }
